4d6991ba5cdd14d5722ee216bf3de398c64a65f6
qalamqas <kalamkas@mail.ru>
2023-10-04 14:28:53 +0600

    Initial Commit

---
+++ MySwiftUIapp/MySwiftUIappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIappApp.swift
+//  MySwiftUIapp
+//
+//  Created by Каламкас on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
