9431102afab46860b8b4526a7bf984fee94f8792
qalamqas <kalamkas@mail.ru>
2023-10-06 18:27:54 +0600

    zcxzxc

--- MySwiftUIapp/ColorsSet.swift
+++ MySwiftUIapp/ColorsSet.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension Array where Element: Equatable {
+    mutating func remove(object: Element) {
+        guard let index = firstIndex(of: object) else {return}
+        remove(at: index)
+    }
+}
--- MySwiftUIapp/ContentView.swift
+++ MySwiftUIapp/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    var colors: [Color]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: [GridItem(.fixed(50)), GridItem(.fixed(50)), GridItem(.fixed(50)), GridItem(.fixed(50))]) {
+            ForEach(0...15, id: \.self) { index in
+                Rectangle()
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(12)
+                    .foregroundColor(colors[index])
+                    .onAppear {
+                        print(colors[index])
+                    }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+//#Preview {
+//    ContentView(colors: [.red])
+//}
+
+extension Color {
+    static func random() -> Color {
+        Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+    }
 }
